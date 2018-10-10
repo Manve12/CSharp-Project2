7f6c797af0180af82195c05ceb3229b699019523
manvidas.barvicius <manvidas.barvicius@NM-16010-PC>
2018-10-10 14:34:58 +0100

    Added blur effect on images

---
+++ WebApp/Scripts/Gallery.js
@@ -0,0 +1,7 @@
+﻿$(".gallery-image img").hover(function () {
+    $(".gallery-image img").css("filter", "blur(5px)");
+    $(this).css("filter","blur(0px)");
+},
+function (){
+    $(".gallery-image img").css("filter", "blur(0px)");
+});
