d7987c3d0907b881f2e30abc8d3286ca7b63e1ae
manvidas.barvicius <manvidas.barvicius@NM-16010-PC>
2018-10-11 12:47:29 +0100

    Updated overlay of gallery images

--- WebApp/Scripts/Gallery.js
+++ WebApp/Scripts/Gallery.js
@@ -1,4 +1,8 @@
 ﻿$(document).ready(function () {
+    //// VARIABLES ////
+    var detailsShown = false;
+
+
     /////////////// PAGE NUMBERS //////////////
 
     //load default page number selection
@@ -15,16 +19,39 @@
     //image blur on hover
     $(".gallery-image img").hover(function () {
         $(".gallery-image img").css("filter", "blur(5px)");
+        $(this).find("img").css("filter", "blur(0px)");
         $(this).css("filter","blur(0px)");
     },
     function () {
         $(".gallery-image img").css("filter", "blur(0px)");
+        $(".gallery-image-overlay-button").parent().prev().css("display", "none");
+        detailsShown = false;
     });
 
     //gallery image view button handling for blur
     $(".gallery-image-overlay").hover(function () {
         $(".gallery-image img").css("filter", "blur(5px)");
+        $(this)
+            .find(".gallery-image-overlay-wrapper")
+            .find(".gallery-image-overlay-author")
+            .find("img")
+            .css("filter", "blur(0px)");
         $(this).prevAll('img').first().css("filter", "blur(0px)");
+        
+    });
+
+    //show/hide the image wrapper (author details etc.)
+    $(".gallery-image-overlay-button").click(function () {
+        if (detailsShown == false) {
+            $(this).parent().prev().css("display", "block");
+        }
+
+        if (detailsShown == true){
+            $(this).parent().prev().css("display", "none");
+        }
+        
+        detailsShown = !detailsShown;
+        console.log(detailsShown);
     });
 
 });
