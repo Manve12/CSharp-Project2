1872e8c9496cd81fdac9822da00e45aa1c37d6f6
manvidas.barvicius <manvidas.barvicius@NM-16010-PC>
2018-10-11 15:58:03 +0100

    Added comment controller

--- WebApp/Controllers/CommentController.cs
+++ WebApp/Controllers/CommentController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,9 +12,63 @@ namespace WebApp.Controllers
     public class CommentController : Controller
     {
         // GET: Comment
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Index(string id)
         {
-            return View();
+            if (id.Length == 0)
+            {
+                return HttpNotFound();
+            }
+
+            try
+            {
+                dynamic photoData = GetPhoto(id);
+
+                dynamic userData = GetUser(photoData["user"]["username"].ToString());
+
+                if (userData == null)
+                {
+                    return HttpNotFound();
+                }
+
+                ViewBag.PhotoData = photoData;
+                ViewBag.UserData = userData;
+
+                return View();
+            }
+            catch (Exception)
+            {
+                return HttpNotFound();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Index(string commentInput, string[] param = null)
+        {
+
+            return HttpNotFound();
+        }
+
+        private static JToken GetPhoto(string id)
+        {
+            try
+            {
+                var url = "https://api.unsplash.com/photos/" + id + "?client_id=451f6851f8873903472e4ae17e9bac7afbe0a6ef4959cdca25b3ca195fdf22d3";
+                var jsonData = new WebClient().DownloadString(url);
+                dynamic obj = JsonConvert.DeserializeObject(jsonData);
+                return obj;
+            } catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private static JToken GetUser(string username)
+        {
+            var url = "https://api.unsplash.com/users/" + username + "?client_id=451f6851f8873903472e4ae17e9bac7afbe0a6ef4959cdca25b3ca195fdf22d3";
+            var jsonData = new WebClient().DownloadString(url);
+            dynamic obj = JsonConvert.DeserializeObject(jsonData);
+            return obj;
         }
     }
 }
