2c3217fc80387f783c9f64d9b329ace09cc611f8
manvidas.barvicius <manvidas.barvicius@NM-16010-PC>
2018-10-11 08:38:00 +0100

    Added dynamic image loading based on page id

--- WebApp/Controllers/GalleryController.cs
+++ WebApp/Controllers/GalleryController.cs
@@ -10,6 +10,7 @@ using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
+using System.Web.UI;
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -19,12 +20,34 @@ namespace WebApp.Controllers
         // GET: Gallery
         public ActionResult Index()
         {
-            
-            ViewBag.PhotoList = GetPhotos();
+            var pageNumber = _getID();
+
+            //depending on the page number work the next set available
+            var listOfPageNumbers = new List<int>();
+
+            for (int i = pageNumber - 4; i < pageNumber + 4; i++)
+            {
+                listOfPageNumbers.Add(i);
+            }
+
+            ViewBag.PhotoList = GetPhotos(pageNumber);
            
             ViewData["RandomImageUrl"] = GetRandomImageUrl();
-
+            ViewData["PageNumbers"] = listOfPageNumbers;
             return View();
+        }
+
+        private static int _getID()
+        {
+            var lastElement = (System.Web.HttpContext.Current.Request.Url.AbsoluteUri).Split('/').Last();
+            int id;
+            bool isNumeric;
+            isNumeric = int.TryParse(lastElement,out id);
+            if (isNumeric)
+            {
+                return id;
+            }
+            return 1;
         }
 
         private static dynamic _callApi(string urlSection, int pageNumber = 1)
@@ -32,7 +55,7 @@ namespace WebApp.Controllers
             var url = "https://api.unsplash.com/" + urlSection + "?page=" + pageNumber + "&client_id=451f6851f8873903472e4ae17e9bac7afbe0a6ef4959cdca25b3ca195fdf22d3";
             var jsonData = new WebClient().DownloadString(url);
             dynamic obj = JsonConvert.DeserializeObject(jsonData);
-
+            
             return obj;
         }
         
@@ -43,7 +66,7 @@ namespace WebApp.Controllers
             return jsonData["urls"]["full"].Value;
         }
 
-        public static List<JToken> GetPhotos(int pageNumber = 1)
+        public static List<JToken> GetPhotos(int pageNumber)
         {
             JArray listOfPhotos = JArray.Parse(_callApi("photos",pageNumber).ToString());
             var photos = new List<JToken>();
