c73e3e92ae0172411f96c0ccb378c92334de5404
manvidas.barvicius <manvidas.barvicius@NM-16010-PC>
2018-10-11 09:24:24 +0100

    Changed the way page numbers are highlighted

--- WebApp/Scripts/Gallery.js
+++ WebApp/Scripts/Gallery.js
@@ -1,23 +1,13 @@
 ﻿$(document).ready(function () {
     //load default page number selection
-    var pNumber = $(".gallery-page-number");
+    var pNumber = $(".gallery-page-number a");
     var lastUrlSegment = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);
-    if (isNaN(lastUrlSegment)) {
-        selectPageNumber(pNumber,1);
-    }
-    else {
-        selectPageNumber(pNumber,lastUrlSegment);
+    if ($.isNumeric(lastUrlSegment)) {
+        $("#" + lastUrlSegment).css("border", "1px solid");
+    } else {
+        $("#1").css("border", "1px solid");
     }
 });
-
-function selectPageNumber(pNumber, pageNumber) {
-    pNumber.each(function (i, obj) {
-        if (i == pageNumber - 1) {
-            $(this).css("border", "1px solid");
-            return;
-        }
-    });
-}
 
 //image blur on hover
 $(".gallery-image img").hover(function () {
@@ -26,10 +16,4 @@ $(".gallery-image img").hover(function () {
 },
 function (){
     $(".gallery-image img").css("filter", "blur(0px)");
-});
-
-//page number selection
-$(".gallery-page-number").click(function () {
-    $(".gallery-page-number").css("border", "none");
-    $(this).css("border", "1px solid");
 });
