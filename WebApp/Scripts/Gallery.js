b409ac4bee52b9b5ff5b25c3634027871649b390
manvidas.barvicius <manvidas.barvicius@NM-16010-PC>
2018-10-11 10:08:31 +0100

    Updated view button on gallery images

--- WebApp/Scripts/Gallery.js
+++ WebApp/Scripts/Gallery.js
@@ -1,4 +1,6 @@
 ﻿$(document).ready(function () {
+    /////////////// PAGE NUMBERS //////////////
+
     //load default page number selection
     var pNumber = $(".gallery-page-number a");
     var lastUrlSegment = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);
@@ -7,13 +9,22 @@
     } else {
         $("#1").css("border", "1px solid");
     }
-});
 
-//image blur on hover
-$(".gallery-image img").hover(function () {
-    $(".gallery-image img").css("filter", "blur(5px)");
-    $(this).css("filter","blur(0px)");
-},
-function (){
-    $(".gallery-image img").css("filter", "blur(0px)");
+    ////////////// BLUR //////////////
+
+    //image blur on hover
+    $(".gallery-image img").hover(function () {
+        $(".gallery-image img").css("filter", "blur(5px)");
+        $(this).css("filter","blur(0px)");
+    },
+    function () {
+        $(".gallery-image img").css("filter", "blur(0px)");
+    });
+
+    //gallery image view button handling for blur
+    $(".gallery-image-overlay").hover(function () {
+        $(".gallery-image img").css("filter", "blur(5px)");
+        $(this).prevAll('img').first().css("filter", "blur(0px)");
+    });
+
 });
